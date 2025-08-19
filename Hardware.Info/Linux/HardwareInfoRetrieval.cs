5b505933b023b73bd660836635354dec8c9403d3
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-08-18 23:50:37 -0400

    chore#: Add example lspci output

--- Hardware.Info/Linux/HardwareInfoRetrieval.cs
+++ Hardware.Info/Linux/HardwareInfoRetrieval.cs
@@ -1100,7 +1100,6 @@ namespace Hardware.Info.Linux
             uint currentRefreshRate = 0;
 
             // xrandr -q
-
             /*
             Screen 0: minimum 320 x 200, current 1280 x 800, maximum 4096 x 4096
             VGA1 disconnected (normal left inverted right x axis y axis)
@@ -1173,6 +1172,50 @@ namespace Hardware.Info.Linux
                     }
                 }
             }
+
+            // lspci -D
+            /*
+            0000:00:00.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Root Complex
+            0000:00:00.2 IOMMU: Advanced Micro Devices, Inc. [AMD] Phoenix IOMMU
+            0000:00:01.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Dummy Host Bridge
+            0000:00:02.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Dummy Host Bridge
+            0000:00:02.2 PCI bridge: Advanced Micro Devices, Inc. [AMD] Phoenix GPP Bridge
+            0000:00:02.4 PCI bridge: Advanced Micro Devices, Inc. [AMD] Phoenix GPP Bridge
+            0000:00:03.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Dummy Host Bridge
+            0000:00:03.1 PCI bridge: Advanced Micro Devices, Inc. [AMD] Family 19h USB4/Thunderbolt PCIe tunnel
+            0000:00:04.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Dummy Host Bridge
+            0000:00:04.1 PCI bridge: Advanced Micro Devices, Inc. [AMD] Family 19h USB4/Thunderbolt PCIe tunnel
+            0000:00:08.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Dummy Host Bridge
+            0000:00:08.1 PCI bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Internal GPP Bridge to Bus [C:A]
+            0000:00:08.2 PCI bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Internal GPP Bridge to Bus [C:A]
+            0000:00:08.3 PCI bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Internal GPP Bridge to Bus [C:A]
+            0000:00:14.0 SMBus: Advanced Micro Devices, Inc. [AMD] FCH SMBus Controller (rev 71)
+            0000:00:14.3 ISA bridge: Advanced Micro Devices, Inc. [AMD] FCH LPC Bridge (rev 51)
+            0000:00:18.0 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 0
+            0000:00:18.1 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 1
+            0000:00:18.2 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 2
+            0000:00:18.3 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 3
+            0000:00:18.4 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 4
+            0000:00:18.5 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 5
+            0000:00:18.6 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 6
+            0000:00:18.7 Host bridge: Advanced Micro Devices, Inc. [AMD] Phoenix Data Fabric; Function 7
+            0000:01:00.0 Network controller: MEDIATEK Corp. MT7922 802.11ax PCI Express Wireless Network Adapter
+            0000:02:00.0 Non-Volatile memory controller: Sandisk Corp WD Black SN850X NVMe SSD (rev 01)
+            0000:c1:00.0 VGA compatible controller: Advanced Micro Devices, Inc. [AMD/ATI] Phoenix1 (rev c4)
+            0000:c1:00.1 Audio device: Advanced Micro Devices, Inc. [AMD/ATI] Radeon High Definition Audio Controller [Rembrandt/Strix]
+            0000:c1:00.2 Encryption controller: Advanced Micro Devices, Inc. [AMD] Phoenix CCP/PSP 3.0 Device
+            0000:c1:00.3 USB controller: Advanced Micro Devices, Inc. [AMD] Device 15b9
+            0000:c1:00.4 USB controller: Advanced Micro Devices, Inc. [AMD] Device 15ba
+            0000:c1:00.5 Multimedia controller: Advanced Micro Devices, Inc. [AMD] Audio Coprocessor (rev 63)
+            0000:c1:00.6 Audio device: Advanced Micro Devices, Inc. [AMD] Family 17h/19h/1ah HD Audio Controller
+            0000:c2:00.0 Non-Essential Instrumentation [1300]: Advanced Micro Devices, Inc. [AMD] Phoenix Dummy Function
+            0000:c2:00.1 Signal processing controller: Advanced Micro Devices, Inc. [AMD] AMD IPU Device
+            0000:c3:00.0 Non-Essential Instrumentation [1300]: Advanced Micro Devices, Inc. [AMD] Phoenix Dummy Function
+            0000:c3:00.3 USB controller: Advanced Micro Devices, Inc. [AMD] Device 15c0
+            0000:c3:00.4 USB controller: Advanced Micro Devices, Inc. [AMD] Device 15c1
+            0000:c3:00.5 USB controller: Advanced Micro Devices, Inc. [AMD] Pink Sardine USB4/Thunderbolt NHI controller #1
+            0000:c3:00.6 USB controller: Advanced Micro Devices, Inc. [AMD] Pink Sardine USB4/Thunderbolt NHI controller #2
+            /**/
 
             processOutput = ReadProcessOutput("lspci", "-D");
 
